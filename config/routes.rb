dbd30a6b99dbe5943244560d8541bfd10754e7e5
morisuke <cyssd1031@gmail.com>
2024-05-01 01:31:03 +0900

    1回目

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  get 'memos/index'
+  get 'memos/show'
+  get 'memos/new'
+  get 'memos/create'
+  get 'memos/edit'
+  get 'memos/update'
+  get 'memos/destroy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  resources :memos
+root to: 'memos#index'
+
+end
